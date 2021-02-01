6126ff537bcd709321bb0f9efcfe8a7934aaf606
tawhipple <tanner.whipple@yahoo.com>
2021-01-31 16:16:22 -0800

    fixed dict file

--- test_dict.py
+++ test_dict.py
@@ -1,3 +1,14 @@
 from getpass import getpass
 from netmiko import ConnectHandler
 
+device1 = {
+    "host": "cisco3.lasthop.io",
+    "username": "pyclass",
+    "password": getpass(),
+    "device_type": "cisco_ios",
+    # "session_log": "my_session.txt",
+}
+
+net_connect = ConnectHandler(**device1)
+print(net_connect.find_prompt())
+
